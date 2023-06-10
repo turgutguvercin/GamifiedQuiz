df93467a00180a58618a465d526a87cd2b31e33a
turgutguvercin <64164223+turgutguvercin@users.noreply.github.com>
2023-06-10 13:40:12 +0100

    Update MainPage.js

--- Front-End/src/pages/MainPage/MainPage.js
+++ Front-End/src/pages/MainPage/MainPage.js
@@ -1,56 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '../../components/Navbar/Navbar';
 import AppMain from '../AppMain/AppMain';
 import '../../App.css';
 
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"/>
-class MainPage extends React.Component {
-
- // create the navbar 
-constructor() {
-  super();
-  this.state = {
+function MainPage() {
+  
+  // create the navbar 
+  const [state, setState] = useState({
     email:'hide',
     logout:'hide',
-    dashboard:"hide"
-    
-  }
-}
+    login: '',
+    signup: '',
+    dashboard:'hide'
+  });
 
-componentDidMount(){
-  axios.get('http://localhost/session.php',{withCredentials: true})
-  .then(response => {
-    console.log(response.data);
-    if(response.data.UserID === "unauthorised")
-    {
-      this.setState ({
-        email:'hide',
-        logout:'hide'
-      })
-    }
-    else{
-      this.setState({
-        email:response.data.Email,
-        logout:'',
-        login:'hide',
-        signup:'hide',
-        dashboard:''
+  useEffect(() => {
+    axios.get('http://localhost/session.php', { withCredentials: true })
+      .then(response => {
+        console.log(response.data);
+        if(response.data.UserID === "unauthorised")
+        {
+          setState(prevState => ({
+            ...prevState,
+            email: 'hide',
+            logout: 'hide'
+          }));
+        }
+        else {
+          setState({
+            email: response.data.Email,
+            logout: '',
+            login: 'hide',
+            signup: 'hide',
+            dashboard: ''
+          });
+        }
+      });
+  }, []);
 
-     })
-    }
-
-  })
-}
-
-render(){
   return (
-    <div >
-      <Navbar user={this.state.email} email={this.state.email}  logout={this.state.logout}
-       login={this.state.login} signup={this.state.signup} dashboard={this.state.dashboard} />
+    <div>
+      <Navbar user={state.email} email={state.email} logout={state.logout} 
+              login={state.login} signup={state.signup} dashboard={state.dashboard} />
       <AppMain/>
-      </div>
+    </div>
   );
 }
-}
+
 export default MainPage;
